0d547fdc6154198acaa7306db7d4416f901dec09
John Gill <swfiua@gmail.com>
2015-01-06 17:16:47 -0400

    team and competitor models

--- django_eighty_days/models.py
+++ django_eighty_days/models.py
@@ -1,21 +1,74 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.conf import settings
+from django.apps import apps
 
+# max length for names in models
+NAME_LENGTH=200
+
+# Get the user model
+#UserModel = apps.get_model(settings.AUTH_USER_MODEL)
+
+# Teams and competitors
+class Competition(models.Model):
+    """ A competition
+
+    Each competition consists of:
+
+    Route
+    Teams
+    Activities
+    Conditional activities: only allowed once you get to a place.
+    """
+    name  = models.CharField(max_length=NAME_LENGTH)
+    description = models.TextField()
+    team_size = models.PositiveIntegerField()
+    
+
+class Competitor(models.Model):
+    """ Competitor is a user competing in a competition """
+    user = models.ForeignKey('users.User')
+    nickname = models.CharField(max_length=NAME_LENGTH)
+    competition = models.ForeignKey('Competition')
+
+class Team(models.Model):
+    """ A team entered in a competition """
+    name = models.CharField(max_length=NAME_LENGTH)
+    captain = models.ForeignKey(Competitor)
+
+class TeamMember(models.Model):
+    """ A team member is a competitor associcated with a team """
+    competitor = models.ForeignKey(Competitor)
+    team = models.ForeignKey(Team)
+
+class TeamMemberRequest(models.Model):
+    """ Handles requests to join a team """
+    team = models.ForeignKey(Team)
+    competitor = models.ForeignKey(Competitor)
+    
+    
+# Places and routes -- should use GTFS
 class Place(models.Model):
     pass
     
 class Route(models.Model):
     pass
-    
-class Competition(models.Model):
-    name  = models.CharField(max_length=200)
-    description = models.TextField()
 
+    
+# Activities
 class Activity(models.Model):
-    name = models.CharField(max_length=200)
-    units = models.CharField(max_length=200)
+    name = models.CharField(max_length=NAME_LENGTH)
+    units = models.CharField(max_length=NAME_LENGTH)
     miles_per_unit = models.FloatField()
     
 class Workout(models.Model):
+    """ Each workout is an activity
+    
+    With a number of units and a date
+    """
     activity = models.ForeignKey('Activity')
+    units = models.FloatField()
+    date = models.DateField()
+
+
