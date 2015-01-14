584371a6fcb4dd272707e9d88b906604318df202
John Gill <swfiua@gmail.com>
2015-01-14 16:54:22 -0400

    autogenerate serializers

--- django_eighty_days/serializers.py
+++ django_eighty_days/serializers.py
@@ -11,4 +11,39 @@ for name, clazz in codegen.get_models():
 
 ]]]"""
 
-#[[[end]]]
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Activity
+
+class CompetitionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Competition
+
+class CompetitorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Competitor
+
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Place
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Route
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Team
+
+class TeamMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.TeamMember
+
+class TeamMemberRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.TeamMemberRequest
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Workout
+#[[[end]]] (checksum: 8d776de98175cbe99f1106b0ae5af988)
