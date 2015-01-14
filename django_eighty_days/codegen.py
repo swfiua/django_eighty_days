1898df77cc7942b1c97e46e82fa3aef2cc526c04
John Gill <swfiua@gmail.com>
2015-01-14 16:48:27 -0400

    remove apigen.py and add codgen.py, new version using cog.

---
+++ django_eighty_days/codegen.py
@@ -0,0 +1,51 @@
+import inspect
+from django.conf import settings
+
+# configure settings
+if not settings.configured:
+    settings.configure(INSTALLED_APPS=['django_eighty_days'])
+
+from django_eighty_days import models
+
+API_TEMPLATE = """
+class %(name)sList(generics.ListCreateAPIView):
+    \"\"\" Create or get %(name)s objects
+    
+    Without a pk, returns all %(name)s objects.
+
+    With a pk, returns just that %(name)s
+    \"\"\"
+    queryset = models.%(name)s.objects.all()
+    serializer_class = serializers.%(name)sSerializer
+"""
+
+SERIALIZE_TEMPLATE = """
+class %(name)sSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.%(name)s
+"""
+
+
+def get_models():
+    """ Return the model classes for this app
+
+    Return a list of 2-tuples: (name, class)
+    """
+    items = inspect.getmembers(models, inspect.isclass)
+
+    return [x for x in items if issubclass(x[1], models.models.Model)]
+
+def get_api_code(name):
+    """ Generate API class for name """
+    return API_TEMPLATE % dict(name=name)
+
+def get_serializer_code(name):
+    """ Generate Serializer class for name """
+    return SERIALIZE_TEMPLATE % dict(name=name)
+
+if __name__ == '__main__':
+
+    from cogapp import Cog
+
+    for filename in ('api.py', 'serializers.py', 'urls.py'):
+        Cog().main(['cog', '-cr', 'django_eighty_days/' + filename])
