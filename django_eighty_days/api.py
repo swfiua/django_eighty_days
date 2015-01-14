1ed7b802d173a590cf286d81b9e8d6583d8191bc
John Gill <swfiua@gmail.com>
2015-01-14 16:54:44 -0400

    autogenerate api classes

--- django_eighty_days/api.py
+++ django_eighty_days/api.py
@@ -5,6 +5,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
+"""[[[cog
+
+from django_eighty_days import codegen
+
+for name, clazz in codegen.get_models():
+
+    cog.out(codegen.get_api_code(name))
+
+]]]"""
+
+class ActivityList(generics.ListCreateAPIView):
+    """ Create or get Activity objects
+    
+    Without a pk, returns all Activity objects.
+
+    With a pk, returns just that Activity
+    """
+    queryset = models.Activity.objects.all()
+    serializer_class = serializers.ActivitySerializer
+
 class CompetitionList(generics.ListCreateAPIView):
     """ Create or get Competition objects
     
@@ -14,7 +34,6 @@ class CompetitionList(generics.ListCreateAPIView):
     """
     queryset = models.Competition.objects.all()
     serializer_class = serializers.CompetitionSerializer
-
 
 class CompetitorList(generics.ListCreateAPIView):
     """ Create or get Competitor objects
@@ -26,6 +45,25 @@ class CompetitorList(generics.ListCreateAPIView):
     queryset = models.Competitor.objects.all()
     serializer_class = serializers.CompetitorSerializer
 
+class PlaceList(generics.ListCreateAPIView):
+    """ Create or get Place objects
+    
+    Without a pk, returns all Place objects.
+
+    With a pk, returns just that Place
+    """
+    queryset = models.Place.objects.all()
+    serializer_class = serializers.PlaceSerializer
+
+class RouteList(generics.ListCreateAPIView):
+    """ Create or get Route objects
+    
+    Without a pk, returns all Route objects.
+
+    With a pk, returns just that Route
+    """
+    queryset = models.Route.objects.all()
+    serializer_class = serializers.RouteSerializer
 
 class TeamList(generics.ListCreateAPIView):
     """ Create or get Team objects
@@ -37,7 +75,6 @@ class TeamList(generics.ListCreateAPIView):
     queryset = models.Team.objects.all()
     serializer_class = serializers.TeamSerializer
 
-
 class TeamMemberList(generics.ListCreateAPIView):
     """ Create or get TeamMember objects
     
@@ -47,7 +84,6 @@ class TeamMemberList(generics.ListCreateAPIView):
     """
     queryset = models.TeamMember.objects.all()
     serializer_class = serializers.TeamMemberSerializer
-
 
 class TeamMemberRequestList(generics.ListCreateAPIView):
     """ Create or get TeamMemberRequest objects
@@ -59,3 +95,13 @@ class TeamMemberRequestList(generics.ListCreateAPIView):
     queryset = models.TeamMemberRequest.objects.all()
     serializer_class = serializers.TeamMemberRequestSerializer
 
+class WorkoutList(generics.ListCreateAPIView):
+    """ Create or get Workout objects
+    
+    Without a pk, returns all Workout objects.
+
+    With a pk, returns just that Workout
+    """
+    queryset = models.Workout.objects.all()
+    serializer_class = serializers.WorkoutSerializer
+#[[[end]]] (checksum: 0bceace600ebefa13f4d33a43652ebd7)
