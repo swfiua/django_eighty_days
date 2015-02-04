25d33f7b8f428ba1419e713cd23ea0dcf378dabc
John Gill <swfiua@gmail.com>
2015-02-04 11:24:13 -0400

    Add Activity and Workout to admin

--- django_eighty_days/admin.py
+++ django_eighty_days/admin.py
@@ -3,6 +3,8 @@ from django_eighty_days import models
 
 admin.site.register(models.Competition)
 admin.site.register(models.Competitor)
+admin.site.register(models.Activity)
+admin.site.register(models.Workout)
 admin.site.register(models.Team)
 admin.site.register(models.TeamMember)
 admin.site.register(models.TeamMemberRequest)
